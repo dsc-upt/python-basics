7575f5c45dc4631a248ff4cb74fc738eaa1716d1
VladS159 <sarbuvlad6@yahoo.com>
2021-11-07 14:40:34 +0200

    am adaugat tema de la basic

---
+++ Sarbu vlad - tema/pb1.py
@@ -0,0 +1,5 @@
+dict = {}
+for i in range(10):
+    name = input("Name: ")
+    dict[name] = int(input("Grade: "))
+print(dict)
---
+++ Sarbu vlad - tema/pb2.py
@@ -0,0 +1,7 @@
+list = [-1, 5, 2, 1.4, 8.9, 6]
+max = -1
+for i in list:
+    if(max < i):
+        max = i
+print(max)
+    
